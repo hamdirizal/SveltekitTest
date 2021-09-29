26200afca63c19a5013dc322374da4a509faba8c
Hamdi Rizal <hamdirizal@gmail.com>
2021-09-29 21:37:59 +0700

    Moved redirects to the static folder

--- src/postbuild.js
+++ src/postbuild.js
@@ -3,22 +3,6 @@ import fs from 'fs';
 import { cwd } from 'process';
 
 async function run(){
-	copyRedirectRulesToTheBuildDirectory();
-}
-
-
-/**
- * Copying redirection rules to the build folder. This file is needed by the Netlify service.
- */
-function copyRedirectRulesToTheBuildDirectory(){
-	let buildFolder = path.join(cwd(), 'build');
-	let originalFile = path.join(cwd(), 'src', '_redirects');
-	let targetFile = path.join(buildFolder,'_redirects');
-	console.info('Copying redirection file ',originalFile, ' => ', targetFile );
-	if(!fs.existsSync( buildFolder )) {
-		fs.mkdirSync( buildFolder )
-	}
-	fs.copyFileSync(originalFile, targetFile , fs.constants.COPYFILE_FICLONE);
 }
 
 run();
